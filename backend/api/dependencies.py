f7b33206a232c00c87f3eb862e0465b5105c481a
Home_desktop <dajung2140@gmail.com>
2025-08-07 22:00:53 +0900

    feat: Dockerize backend and electron-app, refactor JWT handling

--- backend/api/dependencies.py
+++ backend/api/dependencies.py
@@ -1,10 +1,8 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-import jwt
-from jwt import exceptions
 from sqlalchemy.orm import Session
 
-from backend.core.security import SECRET_KEY, ALGORITHM
+from backend.core.security import decode_access_token
 from backend.config.database import get_db
 from backend.services.user import get_user_by_email
 
@@ -16,13 +14,15 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except jwt.exceptions.PyJWTError:
+    
+    decoded_token = decode_access_token(token)
+    if decoded_token is None:
         raise credentials_exception
+
+    username: str = decoded_token.get("sub")
+    if username is None:
+        raise credentials_exception
+
     user = get_user_by_email(db, email=username)
     if user is None:
         raise credentials_exception
