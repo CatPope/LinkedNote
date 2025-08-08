e42652bdc73418ddcd8ec8d7b2b1dbe503006535
Home_desktop <dajung2140@gmail.com>
2025-08-08 23:52:19 +0900

    feat: Electron 애플리케이션 시작 시 트레이로 최소화

--- electron-app/main.js
+++ electron-app/main.js
@@ -12,19 +12,30 @@ function createWindow () {
   })
 
   win.loadFile('index.html')
+  win.hide() // 창 숨기기
 }
 
 app.whenReady().then(() => {
   createWindow()
 
   // 트레이 아이콘 설정
-  tray = new Tray(__dirname + '/assets/icons/icon.png') // 아이콘 경로 설정
+  tray = new Tray(__dirname + '/assets/icons/icon.png')
   const contextMenu = Menu.buildFromTemplate([
     { label: '설정', click: () => { /* 설정 창 열기 로직 */ } },
     { label: '종료', click: () => app.quit() }
   ])
   tray.setToolTip('LinkedNote')
   tray.setContextMenu(contextMenu)
+
+  // 트레이 아이콘 클릭 시 창 토글
+  tray.on('click', () => {
+    const win = BrowserWindow.getAllWindows()[0] // 첫 번째 창 가져오기
+    if (win.isVisible()) {
+      win.hide()
+    } else {
+      win.show()
+    }
+  })
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
