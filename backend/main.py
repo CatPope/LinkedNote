e75d95a4e4c6f9b1922b8cecf1da41fa155b7e35
Home_desktop <dajung2140@gmail.com>
2025-08-09 00:03:36 +0900

    feat: FastAPI main.py에 summarize 라우터 포함

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, Depends
 from .api import auth
 from .api.dependencies import get_current_user
 from .models import User
-from .schemas import user as user_schemas # user 스키마 임포트
+from .schemas import user as user_schemas
+from .api import summarize
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
+app.include_router(summarize.router, prefix="/api", tags=["summarize"])
 
 @app.get("/")
 async def read_root():
