3f612bd652ecea84fe3bd5d75778ce43b882931d
Home_desktop <dajung2140@gmail.com>
2025-08-08 23:54:38 +0900

    feat: Electron 클립보드 URL 유효성 검사 로직 구현

--- electron-app/src/main/clipboard-monitor.js
+++ electron-app/src/main/clipboard-monitor.js
@@ -2,13 +2,26 @@ const { clipboard } = require('electron');
 
 let lastClipboardText = '';
 
+function isValidUrl(text) {
+  try {
+    new URL(text);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 function startMonitoring(interval = 1000) {
   setInterval(() => {
     const currentClipboardText = clipboard.readText();
     if (currentClipboardText && currentClipboardText !== lastClipboardText) {
       lastClipboardText = currentClipboardText;
-      console.log('Clipboard changed:', currentClipboardText);
-      // 여기에 URL 유효성 검사 및 이벤트 발생 로직 추가
+      if (isValidUrl(currentClipboardText)) {
+        console.log('Valid URL detected:', currentClipboardText);
+        // 여기에 URL 감지 시 이벤트 발생 로직 추가
+      } else {
+        console.log('Non-URL text detected:', currentClipboardText);
+      }
     }
   }, interval);
 }
