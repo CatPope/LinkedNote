89b4c934dcbe323ee7ccefc80ee6ab5f52b70508
Home_desktop <dajung2140@gmail.com>
2025-08-08 23:46:17 +0900

    feat: 보호된 경로를 위한 인증 미들웨어 개발

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from .api import auth
+from .api.dependencies import get_current_user
+from .models import User
+from .schemas import user as user_schemas # user 스키마 임포트
 
 app = FastAPI()
 
@@ -8,3 +11,7 @@ app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
 @app.get("/")
 async def read_root():
     return {"message": "Hello, LinkedNote!"}
+
+@app.get("/users/me/", response_model=user_schemas.UserInDB)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
